74ccd2fd7ad0d12f16306de2b534e4797f2ef34a
Nguyễn Đức Huy <nguyenduchuywork23@gmail.com>
2026-07-31 07:01:08 +0700

    Fix: Remove emojis and update scripts

--- scripts/01_iptables_routing.sh
+++ scripts/01_iptables_routing.sh
@@ -8,11 +8,11 @@
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# 1. KILL-SWITCH (Co lap hoan toan ma doc)
+# 1. KILL-SWITCH (Cô lập malware)
 iptables -P FORWARD DROP
 iptables -F && iptables -t nat -F
 
-# 2. NETWORK DECEPTION (Be lai traffic ma doc vao INetSim)
+# 2. NETWORK DECEPTION (Lái malware vào INetSim)
 iptables -t nat -A PREROUTING -i ens19 -p udp --dport 53 -j REDIRECT --to-port 53
 iptables -t nat -A PREROUTING -i ens19 -p tcp --dport 53 -j REDIRECT --to-port 53
 iptables -t nat -A PREROUTING -i ens19 -p tcp --dport 80 -j REDIRECT --to-port 80
@@ -20,7 +20,7 @@ iptables -t nat -A PREROUTING -i ens19 -p tcp --dport 443 -j REDIRECT --to-port 
 iptables -t nat -A PREROUTING -i ens19 -p tcp --dport 21 -j REDIRECT --to-port 21
 iptables -t nat -A PREROUTING -i ens19 -p tcp --dport 25 -j REDIRECT --to-port 25
 
-# 3. LOG PIPELINE (Chi cho phep Wazuh Agent 1514/1515 lien lac ve Server)
+# 3. LOG PIPELINE (Chỉ cho phép Wazuh Agent 1514/1515 liên lạc về Server)
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i ens19 -o ens18 -p tcp -d 192.168.1.244 --dport 1514 -j ACCEPT
 iptables -A FORWARD -i ens19 -o ens18 -p udp -d 192.168.1.244 --dport 1514 -j ACCEPT
